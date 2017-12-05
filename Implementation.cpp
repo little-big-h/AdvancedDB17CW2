d190aa58bc440b8724f439c28996193023df2b84
Holger Pirk <hlgr@dyn1207-145.wlan.ic.ac.uk>
2017-12-05 17:20:54 +0000

    added example answer

--- Implementation.cpp
+++ Implementation.cpp
@@ -1,29 +1,26 @@
 #include "Implementation.hpp"
-#include <iostream>
 
-using namespace std;
 //////////////////// Nested Loop Joins ////////////////////
 
 std::vector<std::string> getQualifyingBusinessesIDsVector(Businesses const& b, float latMin,
 																													float latMax, float longMin,
 																													float longMax) {
-	// This function needs to find all businesses that have within the
-	// specified latitude/longitude range and store their ids in the result vector
-	std::cout << "function getQualifyingBusinessesIDsVector not implemented" << std::endl;
-	throw std::logic_error("unimplemented");
+	std::vector<std::string> qualifyingBusinesses;
+	for(unsigned long i = 0; i < b.ids.size(); i++)
+		if(b.latitudes[i] >= latMin && b.latitudes[i] <= latMax && b.longitudes[i] >= longMin &&
+			 b.longitudes[i] <= longMax)
+			qualifyingBusinesses.emplace_back(b.ids[i]);
+	return qualifyingBusinesses;
 }
 
-std::vector<unsigned long>
-performNestedLoopJoinAndAggregation(Reviews const& r, std::vector<std::string> const& qualifyingBusinessesIDs) {
-	// The second parameter of this function is the vector of qualifying
-	// business ids you have created in the first function
-
-	// This function needs to find all reviews that have business_ids in
-	// the qualifyingBusinessesIDs vector and build a histogram over their stars
-	// The return value is that histogram
-
-	std::cout << "function performNestedLoopJoinAndAggregation not implemented" << std::endl;
-	throw std::logic_error("unimplemented");
+std::vector<unsigned long> performNestedLoopJoinAndAggregation(Reviews const& r,
+																												std::vector<std::string> const& qualies) {
+	std::vector<unsigned long> groups(6);
+	for(unsigned long j = 0; j < qualies.size(); j++)
+		for(unsigned long i = 0; i < r.business_ids.size(); i++)
+			if(r.business_ids[i] == qualies.at(j))
+				groups[r.stars[i]]++;
+	return groups;
 }
 
 //////////////////// Hash Join ////////////////////
@@ -31,21 +28,20 @@ performNestedLoopJoinAndAggregation(Reviews const& r, std::vector<std::string> c
 std::unordered_set<std::string> getQualifyingBusinessesIDs(Businesses const& b, float latMin,
 																													 float latMax, float longMin,
 																													 float longMax) {
-	// This function needs to find all businesses that have within the
-	// specified latitude/longitude range and store their ids in the result set
-	std::cout << "function getQualifyingBusinessesIDs not implemented" << std::endl;
-	throw std::logic_error("unimplemented");
+	std::unordered_set<std::string> qualifyingBusinesses;
+	for(unsigned long i = 0; i < b.ids.size(); i++)
+		if(b.latitudes[i] >= latMin && b.latitudes[i] <= latMax && b.longitudes[i] >= longMin &&
+			 b.longitudes[i] <= longMax)
+			qualifyingBusinesses.insert(b.ids[i]);
+	return qualifyingBusinesses;
 }
 
 std::vector<unsigned long>
 aggregateStarsOfQualifyingBusinesses(Reviews const& r,
 																		 std::unordered_set<std::string> const& qualifyingBusinesses) {
-	// The second parameter of this function is the set of qualifying
-	// business ids you have created in the first function
-
-	// This function needs to find all reviews that have business_ids in
-	// the qualifyingBusinessesIDs vector and build a histogram over their stars
-	// The return value is that histogram
-	std::cout << "function aggregateStarsOfQualifyingBusinesses not implemented" << std::endl;
-	throw std::logic_error("unimplemented");
+	std::vector<unsigned long> groups(6);
+	for(unsigned long i = 0; i < r.business_ids.size(); i++)
+		if(qualifyingBusinesses.count(r.business_ids[i]) > 0)
+			groups[r.stars[i]]++;
+	return groups;
 }
